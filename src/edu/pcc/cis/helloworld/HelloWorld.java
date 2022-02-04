472ee3d3fdee244b9c80b2d7c57e5b980f8ffed6
Mat <harper.matt@pcc.edu>
2022-02-03 16:58:18 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,14 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * A simple hello world program
+ *
+ * Author: Matt Harper
+ * Version: 03FEB2022
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
